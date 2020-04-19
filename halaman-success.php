78a69e80f515b61af57564c9284c4cf257495e3e
Ego Dafma Dasa <egodasa@users.noreply.github.com>
2020-04-19 15:08:25 +0700

    Create halaman-success.php

---
+++ halaman-success.php
@@ -0,0 +1,26 @@
+<?php
+  require "vendor/autoload.php";
+  require_once "google-oauth.php"; // file ini berisi kode google oauth yang sudah diringkas
+  // informasi seperti username, email, foto dll akan dimunculkan di halaman-success.php
+  $google = new GoogleAuth("CLIEN_ID_ANDA", "PRIVATE_KEY", "halaman-success.php");
+  
+  // Token akan otomatis berada di query string code, yang berasal dari halaman-login.php :
+  if(!empty($_GET['code']))
+  {
+    // token yang sudah ada, akan diambil datanya
+    $data = $google->GetDataUser($_GET['code']);
+    
+    // kita cek dulu apaka token valid atau tidak
+    if($data['error'] == true)
+    {
+      // munculkan error jika ada error
+      echo $data['message'];
+    }
+    else
+    {
+      // jika tidak error, tampilkan data user dalam bentuk json
+      // data user tersimpan di variabel $data['data'] dalam bentuk array asosiatif
+      echo json_encode($data['data']);
+    }
+  }
+?>
